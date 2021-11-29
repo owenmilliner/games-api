7f6a36b871e51f3848f530cf0ff2b08b4168319d
owenmilliner <owenmilliner1@gmail.com>
2021-11-29 14:26:21 +0000

    Refactor of insertData into separate utility function. Fixing of then nesting.

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,74 +1,10 @@
 const db = require('../connection');
-const format = require('pg-format');
-const { dropTables, createTables } = require('../utils');
+const { dropTables, createTables, insertData } = require('../utils');
 
 const seed = async data => {
-  const { categoryData, commentData, reviewData, userData } = data;
-
   await dropTables();
   await createTables();
-
-  const queryString = format(
-    `
-  INSERT INTO categories 
-  (slug, description)
-  VALUES 
-  %L;`,
-    categoryData.map(category => [category.slug, category.description])
-  );
-
-  return db.query(queryString).then(() => {
-    const queryString = format(
-      `
-          INSERT INTO users
-          (username, avatar_url, name)
-          VALUES
-          %L;
-          `,
-      userData.map(user => [user.username, user.avatar_url, user.name])
-    );
-
-    return db.query(queryString).then(() => {
-      const queryString = format(
-        `
-          INSERT INTO reviews
-          (title, review_body, designer, review_img_url, votes, category, owner, created_at)
-          VALUES
-          %L;
-          `,
-        reviewData.map(review => [
-          review.title,
-          review.review_body,
-          review.designer,
-          review.review_img_url,
-          review.votes,
-          review.category,
-          review.owner,
-          review.created_at
-        ])
-      );
-
-      return db.query(queryString).then(() => {
-        const queryString = format(
-          `
-            INSERT INTO comments
-            (author, review_id, votes, created_at, body)
-            VALUES
-            %L;
-            `,
-          commentData.map(comment => [
-            comment.author,
-            comment.review_id,
-            comment.votes,
-            comment.created_at,
-            comment.body
-          ])
-        );
-
-        return db.query(queryString);
-      });
-    });
-  });
+  await insertData(data);
 };
 
 module.exports = seed;
--- db/utils.js
+++ db/utils.js
@@ -1,3 +1,4 @@
+const format = require('pg-format');
 const db = require('./connection');
 
 exports.dropTables = () => {
@@ -55,5 +56,75 @@ exports.createTables = () => {
             created_at DATE DEFAULT CURRENT_TIMESTAMP,
             body VARCHAR(1000) NOT NULL
             );`);
+    });
+};
+
+exports.insertData = data => {
+  const { categoryData, commentData, reviewData, userData } = data;
+
+  const queryString = format(
+    `
+      INSERT INTO categories 
+      (slug, description)
+      VALUES 
+      %L;`,
+    categoryData.map(category => [category.slug, category.description])
+  );
+
+  return db
+    .query(queryString)
+    .then(() => {
+      const queryString = format(
+        `
+              INSERT INTO users
+              (username, avatar_url, name)
+              VALUES
+              %L;
+              `,
+        userData.map(user => [user.username, user.avatar_url, user.name])
+      );
+
+      return db.query(queryString);
+    })
+    .then(() => {
+      const queryString = format(
+        `
+              INSERT INTO reviews
+              (title, review_body, designer, review_img_url, votes, category, owner, created_at)
+              VALUES
+              %L;
+              `,
+        reviewData.map(review => [
+          review.title,
+          review.review_body,
+          review.designer,
+          review.review_img_url,
+          review.votes,
+          review.category,
+          review.owner,
+          review.created_at
+        ])
+      );
+
+      return db.query(queryString);
+    })
+    .then(() => {
+      const queryString = format(
+        `
+                INSERT INTO comments
+                (author, review_id, votes, created_at, body)
+                VALUES
+                %L;
+                `,
+        commentData.map(comment => [
+          comment.author,
+          comment.review_id,
+          comment.votes,
+          comment.created_at,
+          comment.body
+        ])
+      );
+
+      return db.query(queryString);
     });
 };
