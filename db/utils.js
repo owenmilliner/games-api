6b82a9e9996cf52ad310dd7cf84dbb7c01a50569
owenmilliner <owenmilliner1@gmail.com>
2021-11-29 13:00:04 +0000

    Insertion of data into tables(categories, users, reviews, and comments) completed.

--- db/utils.js
+++ db/utils.js
@@ -19,15 +19,15 @@ exports.createTables = () => {
     .query(
       `
     CREATE TABLE categories (
-      slug VARCHAR(50) NOT NULL PRIMARY KEY,
-      description VARCHAR(400)
+      slug VARCHAR(100) NOT NULL PRIMARY KEY,
+      description VARCHAR(1000)
     );`
     )
     .then(() => {
       return db.query(`
         CREATE TABLE users (
-          username VARCHAR(50) NOT NULL PRIMARY KEY,
-          avatar_url VARCHAR(400),
+          username VARCHAR(100) NOT NULL PRIMARY KEY,
+          avatar_url VARCHAR(1000),
           name VARCHAR(100)
         );`);
     })
@@ -35,13 +35,13 @@ exports.createTables = () => {
       return db.query(`
         CREATE TABLE reviews (
           review_id SERIAL PRIMARY KEY,
-          title VARCHAR(50) NOT NULL,
-          review_body VARCHAR(400) NOT NULL,
+          title VARCHAR(100) NOT NULL,
+          review_body VARCHAR(1000) NOT NULL,
           designer VARCHAR(100),
           review_img_url VARCHAR(300) DEFAULT 'https://images.pexels.com/photos/163064/play-stone-network-networked-interactive-163064.jpeg',
           votes INT DEFAUlT 0,
-          category VARCHAR(50) NOT NULL REFERENCES categories(slug),
-          owner VARCHAR(50) NOT NULL REFERENCES users(username),
+          category VARCHAR(100) NOT NULL REFERENCES categories(slug),
+          owner VARCHAR(100) NOT NULL REFERENCES users(username),
           created_at DATE DEFAULT CURRENT_TIMESTAMP
         );`);
     })
@@ -49,11 +49,11 @@ exports.createTables = () => {
       return db.query(`
           CREATE TABLE comments (
             comment_id SERIAL PRIMARY KEY,
-            author VARCHAR(50) NOT NULL REFERENCES users(username),
+            author VARCHAR(100) NOT NULL REFERENCES users(username),
             review_id INT NOT NULL REFERENCES reviews(review_id),
             votes INT DEFAUlT 0,
             created_at DATE DEFAULT CURRENT_TIMESTAMP,
-            body VARCHAR(400) NOT NULL
+            body VARCHAR(1000) NOT NULL
             );`);
     });
 };
