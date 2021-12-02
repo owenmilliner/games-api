63d1362123b6d6424a9104a3c82d0944ddbd2202
owenmilliner <owenmilliner1@gmail.com>
2021-12-02 13:00:01 +0000

    Heroku attempt 7.

--- routers/base.router.js
+++ routers/base.router.js
@@ -1,5 +1,6 @@
 const { welcome } = require('../controllers/api.controller');
 const baseRouter = require('express').Router();
+const apiRouter = require('./api.router');
 
 baseRouter.use('/api', apiRouter);
 
