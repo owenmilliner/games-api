4fd61d1b6a78ae0ab24500af47ef1fe243db6996
owenmilliner <owenmilliner1@gmail.com>
2022-01-11 11:26:03 +0000

    Spelling error in error handling caused an error (the irony).

--- models/error-handling/manage-errors.js
+++ models/error-handling/manage-errors.js
@@ -31,73 +31,73 @@ exports.rejectIfInvalidQueryParameter = (queries) => {
   const category = queries.category.toLowerCase();
 
   if (
-    !["created_at", "owner", "review_id", "category", "votes"].includes(sort)
+    !['created_at', 'owner', 'review_id', 'category', 'votes'].includes(sort)
   ) {
     return Promise.reject({
       errorCode: 400,
-      errorMessage: "Invalid sort parameter.",
+      errorMessage: 'Invalid sort parameter.',
     });
   }
-  if (!["ASC", "DESC"].includes(order)) {
+  if (!['ASC', 'DESC'].includes(order)) {
     return Promise.reject({
       errorCode: 400,
-      errorMessage: "Invalid order parameter.",
+      errorMessage: 'Invalid order parameter.',
     });
   }
   if (
     ![
-      "%",
-      "dexterity",
-      "euro game",
-      "social deduction",
+      '%',
+      'dexterity',
+      'euro game',
+      'social deduction',
       "children's games",
-      "strategy",
-      "hidden-roles",
-      "dexterity",
-      "push-our-luck",
-      "roll-and-write",
-      "deck-building",
-      "engine-building",
+      'strategy',
+      'hidden-roles',
+      'dexterity',
+      'push-your-luck',
+      'roll-and-write',
+      'deck-building',
+      'engine-building',
     ].includes(category)
   ) {
     return Promise.reject({
       errorCode: 400,
-      errorMessage: "Invalid category parameter.",
+      errorMessage: 'Invalid category parameter.',
     });
   }
   if (queries.limit < 1 || queries > 50) {
     return Promise.reject({
       errorCode: 400,
       errorMessage:
-        "Out of bounds for limit parameter. Must be a number between 1 and 50 (inclusive).",
+        'Out of bounds for limit parameter. Must be a number between 1 and 50 (inclusive).',
     });
   }
 };
 
 exports.rejectIfInvalidProperties = (review) => {
   if (
-    !review.hasOwnProperty("owner") ||
-    !review.hasOwnProperty("title") ||
-    !review.hasOwnProperty("review_body") ||
-    !review.hasOwnProperty("designer") ||
-    !review.hasOwnProperty("category")
+    !review.hasOwnProperty('owner') ||
+    !review.hasOwnProperty('title') ||
+    !review.hasOwnProperty('review_body') ||
+    !review.hasOwnProperty('designer') ||
+    !review.hasOwnProperty('category')
   ) {
     return Promise.reject({
       errorCode: 400,
       errorMessage:
-        "Missing property in review object. Must contain: owner, title, review_body, designer and category.",
+        'Missing property in review object. Must contain: owner, title, review_body, designer and category.',
     });
   } else if (
-    typeof review.owner !== "string" ||
-    typeof review.title !== "string" ||
-    typeof review.review_body !== "string" ||
-    typeof review.designer !== "string" ||
-    typeof review.category !== "string"
+    typeof review.owner !== 'string' ||
+    typeof review.title !== 'string' ||
+    typeof review.review_body !== 'string' ||
+    typeof review.designer !== 'string' ||
+    typeof review.category !== 'string'
   ) {
     return Promise.reject({
       errorCode: 400,
       errorMessage:
-        "Invalid data type in review object. Each value must be passed as a string.",
+        'Invalid data type in review object. Each value must be passed as a string.',
     });
   }
 };
